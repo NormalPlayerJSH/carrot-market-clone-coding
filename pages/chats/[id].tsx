ff8e4dd7406c2f09bb155dac43509246a62e11c6
NormalPlayerJSH <jsh010904@gmail.com>
2022-08-05 05:37:05 +0900

    테일윈드 연

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,129 @@
+import type { NextPage } from "next";
+import Layout from "../../components/layout";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <Layout title="채팅" hasTabBar canGoBack >
+      <div className="px-4 space-y-4">
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+          <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+        </div>
+      </div>
+      <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+          <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+        </div>
+      </div>
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+          <p>미쳤어</p>
+        </div>
+      </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>미쳤어</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>미쳤어</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>미쳤어</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>미쳤어</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>Hi how much are you selling them for? Hi how much are you selling them for? Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-400" />
+          <div className="w-3/5 text-sm text-gray-700 p-2 border rounded-md border-gray-300">
+            <p>미쳤어</p>
+          </div>
+        </div>
+        <div className="sticky w-full mx-auto max-w-md bottom-20 inset-x-0">
+          <div className="flex items-center relative">
+            <input type="text" className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500" />
+            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+              <button className=" focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none flex items-center bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600 cursor-pointer">&rarr;</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ChatDetail;
